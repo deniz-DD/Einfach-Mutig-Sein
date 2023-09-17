b612c3bce2b51318a6657735cdbe6a8ab7dc9e88
Deniz <deniz_dural@yahoo.com>
2023-09-17 20:06:04 +0200

    created the imprint-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { LerncoachingComponent } from './lerncoaching/lerncoaching.component';
 import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 const routes: Routes = [
   {path:'' , component: HomeComponent} , 
   {path: 'ueber-mich' , component: AboutMeComponent} , 
   {path: 'lerncoaching' , component: LerncoachingComponent} , 
   {path: 'blog' , component: BlogComponent} , 
-  {path: 'kontakt' , component: ContactComponent}
+  {path: 'kontakt' , component: ContactComponent}, 
+  {path: 'impressum', component: ImprintComponent}
 ];
 
 @NgModule({
