7475015e88e4ffece6fc7d06e43701816c24f3f8
Deniz <deniz_dural@yahoo.com>
2024-07-26 00:02:52 +0200

    added more with ngStyle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,15 +8,36 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   quote_text= "Hallo ich bin der Quote_Text";
 
-  buttonCo: {[key:string]: string}={
-    'background-color': 'blue'
-  };
-
-  red(){
-    if(this.buttonCo['background-color'] === 'blue'){
-      this.buttonCo['background-color'] = 'red';
-    }
-    
-    
+  cardStyle: {[key:string]: string} ={
+    'background-color': 'none', 
+    'height': 'auto',
+    'width': '18rem'
   }
+
+  // buttonCo: {[key:string]: string}={
+  //   'background-color': 'blue',
+  //   'height': '10rem '
+  // };
+
+  // red(){
+  //   if(this.buttonCo['background-color'] === 'blue'){
+  //     this.buttonCo['background-color'] = 'red';
+  // }else{
+  //   this.buttonCo['background-color'] = 'blue'
+  // }
+    
+    
+  // }
+  ReSize(){
+    if(this.cardStyle['width'] === '18rem'){
+      this.cardStyle['height'] = '100%';
+      this.cardStyle['width'] = '100%';
+      
+    }else{
+      
+    }
+
+  }
+
+
 }
